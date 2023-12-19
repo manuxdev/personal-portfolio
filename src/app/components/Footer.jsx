fac8920c13f09b39b32ab975618dde194a5aec17
Manuel Pantoja <manuxdev1010@gmail.com>
2023-12-19 15:50:36 -0500

    footer-fix

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="footer border-t border-t-[#33353F]  ">
       <div className="container p-6 flex justify-between">
-        <span>Logo</span>
+        <span>Manuxdev</span>
         <p className="text-slate-500">All rights reserved</p>
       </div>
     </footer>
